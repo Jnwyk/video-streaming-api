9a7dfc0df6274ef37c41f520d538899490db567b
Jnwyk <nowak.jan@yahoo.com>
2023-11-22 15:23:04 +0100

    add middleware to create a user

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -5,8 +5,14 @@ const get = (req, res, next) => {
   res.status(200).json({ message: "get" });
 };
 
-const create = (req, res, next) => {
-  res.status(200).json({ message: "get" });
+const create = async (req, res, next) => {
+  try {
+    const user = await User.create({ ...req.body });
+    res.status(201).json({ success: true, message: "User created" });
+  } catch (err) {
+    if (err instanceof mongoose.Error.ValidationError) err.statusCode = 400;
+    next(err); // pass error to error middleware
+  }
 };
 
 const update = (req, res, next) => {
