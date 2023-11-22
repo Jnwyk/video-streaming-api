30de2bf00b420a1bb4bab4ad3cec3c3538301ad0
Jan Nowak <nowak.jan@yahoo.com>
2023-11-22 15:53:59 +0100

    add middleware to get a specified user

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,8 +1,22 @@
 const mongoose = require("mongoose");
 const User = require("../models/user.model.js");
+const { IdNotFoundError, NotAllowedError } = require("../errors/errors.js");
 
-const get = (req, res, next) => {
-  res.status(200).json({ message: "get" });
+const get = async (req, res, next) => {
+  try {
+    const userId = req.params.userId;
+    const user = await User.findById(userId);
+    if (!user) {
+      throw new IdNotFoundError(); // throw an error, if stream doesn't exist
+    }
+    if (user.streams.length > 3) {
+      throw new NotAllowedError(user.id); // throw an error, if is watching more than 3 streams
+    }
+    res.status(200).json({ success: true, user: user });
+  } catch (err) {
+    if (err instanceof mongoose.Error.CastError) err = new IdNotFoundError(); // if mongoose throws an error change to IdNotFoundError
+    next(err); // pass error to error middleware
+  }
 };
 
 const create = async (req, res, next) => {
